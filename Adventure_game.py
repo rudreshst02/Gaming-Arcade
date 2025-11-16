51fc6b67d533c32c1b2c1659c47b50f4932d184e
realmayankkr-dev <realmayank.kr@gmail.com>
2025-11-16 23:47:02 +0530

    Create Adventure_game.py

---
+++ Adventure_game.py
@@ -0,0 +1,11 @@
+print('Welcome to our spooky adventure game!!\n\nYou are going to your home in a dark night and your car had some problem in the a lonely road.\nWhile you were analysing the situation another car came and stopped by to check on you.\nHe was an old man probabily in his 60s\nHe offered you a lift and you have TWO choices :\n\nACCEPT it or REJECT it\n')
+
+choice1 = str(input('Enter yes or no for the answer :'))
+
+if choice1 == 'yes':
+    print("He was the trinity killer and he killed you 💀 game over!!")
+
+elif choice1 == 'No':
+    print("You said yes and now regretting your decision")
+
+
