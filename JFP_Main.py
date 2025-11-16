354016dc36e0c8ea83aa704187e6ebf5ed2d4196
RiyanRickson <riyanrickson@gmail.com>
2025-11-16 22:07:56 +0530

    updated JFP main

--- JFP_Main.py
+++ JFP_Main.py
@@ -1,9 +1,12 @@
 ############################################################
-#----------------------------------------------------------#
-#   👾      🎊    WELCOME TO THE GAMING-ARCADE    🎊   🐇 #
-#      ⚔️        ✂️             🎩            🎲          #
-#----------------------------------------------------------#
-############################################################
+print("👾      🎊    WELCOME TO THE GAMING-ARCADE    🎊   🐇 ")
+print("   ⚔️        ✂️             🎩            🎲          ")
+
+ #
+
+##########################################################
+#importing dice game
+import Dice
 
 CHOOSE_THE_GAME_BUTTON = int(input("WELCOME!!\n" "1 --> ROCK-PAPER-SCISSORS\n" "2 --> DICE ROLL\n" "3 --> GUESS THE NUMBER??!!\n\n" "ENTER YOUR CHOICE: " ))
 
@@ -11,7 +14,7 @@ if CHOOSE_THE_GAME_BUTTON == 1:
     pass
 
 elif CHOOSE_THE_GAME_BUTTON == 2:
-    pass
+    Dice.dice_game()
 
 elif CHOOSE_THE_GAME_BUTTON == 3:
     pass
